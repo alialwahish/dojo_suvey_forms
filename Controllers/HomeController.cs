d56128c728b292e656705cdd0c192553d5df3293
Ali Bayati <bayati.ali@icloud.com>
2018-07-06 19:03:58 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcDojSurv.Models;
+
+namespace MvcDojSurv.Controllers
+{
+    public class HomeController : Controller
+    {
+        Ninja ninja=new Ninja(){
+            
+        };
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(ninja);
+        }
+        [HttpPost]
+        [Route("result")]
+        public IActionResult Details(string name,string dojo_location, string favorite_language, string comment){
+            ninja.Name=name;
+            ninja.Location=dojo_location;
+            ninja.Language=favorite_language;
+            ninja.Comment=comment;
+            return View(ninja);
+        }
+
+
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Ninja.cs
@@ -0,0 +1,12 @@
+namespace MvcDojSurv
+{
+
+    public class Ninja
+    {
+
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Language { get; set; }
+        public string Comment { get; set; }
+    }
+}
